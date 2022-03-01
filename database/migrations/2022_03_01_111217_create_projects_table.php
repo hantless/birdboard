8970f70e27d58726d71e0414a1ade123ac235213
Frédéric Arijs <frederic.arijs@ext.bewan.be>
2022-03-01 16:49:37 +0100

    Adding authentification, a project requires an owner

--- database/migrations/2022_03_01_111217_create_projects_table.php
+++ database/migrations/2022_03_01_111217_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
             $table->string('title');
             $table->text('description');
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,10 @@ Route::get('/', function () {
 
 Route::get('/projects', [ProjectsController::class, 'index']);
 Route::get('/projects/{project}', [ProjectsController::class, 'show']);
-Route::post('/projects', [ProjectsController::class, 'store']);
+Route::post('/projects', [ProjectsController::class, 'store'])->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
